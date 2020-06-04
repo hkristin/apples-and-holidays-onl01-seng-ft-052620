e0d9565988194c5d4946f6bd2118cca169b444d4
Kristin Howard <hkristin@andrew.cmu.edu>
2020-06-04 13:44:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,6 +81,7 @@ def all_holidays_with_bbq(holiday_hash)
       return_array << holiday if supply.include?("BBQ")
     end
   end
+  return_array
 end
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
